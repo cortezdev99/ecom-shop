cdae717923d39ab01991f192bf11d31b777a02a2
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-16 21:04:56 -0600

    Built sign in and sign up routes and components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,20 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./components/app";
 import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+import history from './history';
 
 import "./style/main.scss";
+import Signin from './components/auth/Signin'
+// import Signup from './components/auth/Signup'
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <BrowserRouter history={history}>
         <App>
-          
+          <Switch>
+            <Route path="/" component={Signin}/>
+          </Switch>
         </App>
       </BrowserRouter>
     </Provider>,
---
+++ src/components/auth/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Signin extends Component {
+  render() {
+    return (
+      <div className='sign-in'>
+        Sign in!
+      </div>
+    )
+  }
+}
+
+export default Signin
