0d1f7dd826ba7c3d620581eb21cd8e58322de010
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-21 12:52:29 -0600

    setup cart product modal and action creator to fetch and set user cart products

--- src/components/shop/ShopCart.js
+++ src/components/shop/ShopCart.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 import CartProduct from './CartProduct'
+import { connect } from 'react-redux';
+import * as actions from '../../actions'
 
 function CartButton({className, icon}) {
   return (
@@ -12,7 +14,7 @@ function CartButton({className, icon}) {
 
 function CartContent({ className, products }) {
   let count = products.length
-  let productsJSX = products.map(product => <CartProduct key={product} />)
+  let productsJSX = products.map(product => <CartProduct key={product._id} />)
   return (
     <div className={`${className} cart-content`}>
       <div className="cart-content__title">
@@ -50,15 +52,25 @@ function CartFooter({className, products}) {
 }
 
 class ShopCart extends Component {
+  componentDidMount() {
+    this.props.fetchCartProducts()
+  }
   render() {
     const { className } = this.props
     return (
       <div className={`${className} shop-cart`}>
         <CartButton className='shop-cart__toggle' icon='fas fa-times' />
-        <CartContent className='shop-cart__content' products={[4, 32, 453, 243, 443, 526, 63534, 323, 4513, 2433, 4143, 5256, 63253]}/>
+        <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
       </div>
     )
   }
 }
 
-export default ShopCart
+const mapStateToProps = (state) => {
+  const { cartProducts } = state.user
+  return {
+    cartProducts
+  }
+}
+
+export default connect(mapStateToProps, actions)(ShopCart)
