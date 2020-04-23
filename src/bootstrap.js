744fb20973db612fce44038e0ce6636e85052277
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-22 19:18:41 -0600

    brought in firebase and set up my firebase config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { Router, Switch, Route } from "react-router-dom";
 import App from "./components/app";
 import reducers from "./reducers";
+import thunk from 'redux-thunk';
 
 const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 import history from './history';
@@ -20,7 +21,7 @@ import Payment from "./components/information/Payment";
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={createStoreWithMiddleware(reducers, applyMiddleware(thunk))}>
       <Router history={history}>
         <App>
           <Switch>
